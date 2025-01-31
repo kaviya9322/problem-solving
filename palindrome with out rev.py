034c75574541db26f6853682f145f6e0a6b9f719
kaviya k <kamireddykavya2004@gmail.com>
2025-01-31 06:04:13 +0530

    Create palindrome with out rev.py

---
+++ palindrome with out rev.py
@@ -0,0 +1,14 @@
+s = "ABCCBA"
+n = len(s)//2
+mu = len(s)
+valid = True
+for i in range(n):
+  li = s[i]
+  r = s[mu-1-i]
+  if l!=r
+  valid = False
+  break
+  if valid:
+    print("yes")
+  else:
+    print("no")
