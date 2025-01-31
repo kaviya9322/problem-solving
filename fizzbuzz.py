454e43a13f3be309cc08035d75c82b25287ddbc1
kaviya k <kamireddykavya2004@gmail.com>
2025-01-31 08:26:49 +0530

    Create fizzbuzz.py

---
+++ fizzbuzz.py
@@ -0,0 +1,14 @@
+def fizzBuzz(n):
+    ans = []
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            ans.append("FizzBuzz")
+        elif i % 3 == 0:
+            ans.append("Fizz")
+        elif i % 5 == 0:
+            ans.append("Buzz")
+        else:
+            ans.append((i))  
+    return ans
+n=int(input("Enter the num")) 
+print(fizzBuzz(n))
