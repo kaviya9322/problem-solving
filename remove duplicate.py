3355c26cb3264702bc4f635e4ef1433e462bbed7
kaviya k <kamireddykavya2004@gmail.com>
2025-01-31 06:14:20 +0530

    Create remove duplicate.py

---
+++ remove duplicate.py
@@ -0,0 +1,6 @@
+my_list = [1,2,3,1,2]
+new_list = []          # create empty set
+for i in range my_list:      
+  if i not in new_list[]:   # store a values not in my_list
+   new_list.append(i)      
+print(new_list)
