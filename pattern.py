3da9e8428e4ba79d075da03d7683f46c054f5ae2
kaviya k <kamireddykavya2004@gmail.com>
2025-02-07 05:32:18 +0530

    pattern.py

---
+++ pattern.py
@@ -0,0 +1,13 @@
+n=7
+for i in range(n):
+    for j in range(n-i-1):
+        print(" ",end="")
+    for k in range(i+1):
+        print("*",end=" ")
+    print()
+for i in range(n-1,-1,-1):
+    for j in range(n-i-1):
+        print(" ",end="")
+    for k in range(i+1):
+        print("*",end=" ")
+    print()
