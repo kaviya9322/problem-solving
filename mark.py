858ba75ecbfac16e0f7b6cf4931f66aa08ccce7e
kaviya k <kamireddykavya2004@gmail.com>
2025-02-08 19:29:51 +0530

    Create mark.py

---
+++ mark.py
@@ -0,0 +1,11 @@
+n = int(input("enter the number"))
+if n>=90:
+    print("A")
+elif 80<= n <= 89:
+    print("B")
+elif 70<= n <=79:
+    print("c")
+elif 60<= n <=69:
+    print("D")
+else:
+    print("F")
