0cd434c09909906e99b66dbd358e6f0b6b45b3c8
kaviya k <kamireddykavya2004@gmail.com>
2025-02-09 08:23:23 +0530

    Create age group.py

---
+++ age group.py
@@ -0,0 +1,9 @@
+n = int (input("enter the month number"))
+if  n>50:
+    print("above 50")
+elif 40< n <50:
+    print("between 40 to 50")
+elif n<40:
+    print("less then 40")
+else:
+    print("enter valid age")
