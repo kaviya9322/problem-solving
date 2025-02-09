c9fdbfd8865ad16f8b691e5c6d65a3b99cd8ec7a
kaviya k <kamireddykavya2004@gmail.com>
2025-02-09 08:04:46 +0530

    Create season.py

---
+++ season.py
@@ -0,0 +1,11 @@
+n = int (input("enter the month number"))
+if  3<= n <=5:
+    print("spring season")
+elif 6<= n <=8:
+    print("summer season")
+elif 9<= n <=11:
+    print("autumn season")
+elif 9<= n <=11:
+    print("winter season")
+else:
+    print(“Invalid Month Entered”)
